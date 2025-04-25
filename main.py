130ce30485f8b00b655b47381ec8543dc82a2360
Cwaniak <97480309+CwaniaczekCwl@users.noreply.github.com>
2025-04-25 10:10:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
 import discord
 import os
 import asyncio
-import random
 
 intents = discord.Intents.default()
-intents.message_content = True
 intents.guilds = True
-intents.guild_messages = True
+intents.message_content = True
 
 client = discord.Client(intents=intents)
 
 colors = [
-    0xFF0000,  # czerwony
-    0xFF7F00,  # pomarańczowy
-    0xFFFF00,  # żółty
-    0x00FF00,  # zielony
-    0x0000FF,  # niebieski
-    0x4B0082,  # indygo
-    0x8B00FF   # fioletowy
+    0xff0000, 0xff3300, 0xff6600, 0xff9900, 0xffcc00,
+    0xffff00, 0xccff00, 0x99ff00, 0x66ff00, 0x33ff00,
+    0x00ff00, 0x00ff33, 0x00ff66, 0x00ff99, 0x00ffcc,
+    0x00ffff, 0x00ccff, 0x0099ff, 0x0066ff, 0x0033ff,
+    0x0000ff, 0x3300ff, 0x6600ff, 0x9900ff, 0xcc00ff,
+    0xff00ff, 0xff00cc, 0xff0099, 0xff0066, 0xff0033
 ]
 
 running = True
@@ -30,10 +27,12 @@ async def change_role_color():
     guild = client.get_guild(guild_id)
     role = guild.get_role(role_id)
 
+    i = 0
     while running:
-        color = random.choice(colors)
+        color = colors[i % len(colors)]
         await role.edit(colour=discord.Colour(color))
-        await asyncio.sleep(5)  # zmiana koloru co 5 sekund
+        i += 1
+        await asyncio.sleep(0.7)  
 
 @client.event
 async def on_ready():
